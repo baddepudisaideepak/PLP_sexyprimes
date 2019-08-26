d03a5057e4150a20883dadf6ac8d6459490eb2cc
saideepak <baddepudisaideepak@gmail.com>
2019-08-26 17:38:26 +0530

    Initial Commit

---
+++ PLP_sexyprimes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PLP_sexyprimes
+//
+//  Created by Sai Deepak   on 26/08/19.
+//  Copyright © 2019 Sai Deepak  . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
